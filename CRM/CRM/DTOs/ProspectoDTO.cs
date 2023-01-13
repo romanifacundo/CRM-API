335c297891d306794057ec4cc6b02a046ea3cfbd
Facundo-Romani <facundoromani90@gmail.com>
2023-01-13 20:08:26 -0300

    New

--- CRM/CRM/DTOs/ProspectoDTO.cs
+++ CRM/CRM/DTOs/ProspectoDTO.cs
@@ -5,5 +5,6 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string UrlPerfil { get; set; }
+        public List<AgenteDTO> Agentes { get; set; }
     }
 }
